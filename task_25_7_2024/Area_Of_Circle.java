eadd5908860907e4fb47cbd5c24c7b2f878a8bfa
HIMANSHUKAVA <162406359+HIMANSHUKAVA@users.noreply.github.com>
2024-12-07 12:16:06 +0530

    Delete task_25_7_2024 directory

--- task_25_7_2024/Area_Of_Circle.java
+++
@@ -1,9 +0,0 @@
-package task_25_7_2024;
-
-public class Area_Of_Circle {
-
-	public static void main(String[] args) {
-		float r = 18f;
-		System.out.println("Area Of Circle Is :"+(3.14 * r*r));
-	}
-}
